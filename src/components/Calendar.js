7f81143a5a0574242d2d2ed8596610ab8c63eb35
EHEH <81672416+eunhoya@users.noreply.github.com>
2022-05-04 19:11:10 +0900

    first commit

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import FullCalendar, { formatDate } from '@fullcalendar/react'; // must go before plugins
+import FullCalendar, { formatDate, mapHash } from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -12,38 +12,29 @@ const Calendar = () => {
     day: 'numeric',
   });
 
-  useEffect(() => {
-    const getdata = async () => {
-      const response = await axios.get('http://localhost:8081/api/planList');
+  // useEffect(() => {
+  //   const getdata = async () => {
+  //     const response = await axios.get('http://localhost:8081/api/planList');
 
-      console.log('res2', response);
-    };
-    getdata();
-  }, []);
-
-  axios
-    .get('http://localhost:8081/api/planList')
-    .then((Response) => {
-      console.log('res', Response);
-    })
-    .catch((Error) => {
-      console.log('getError', Error);
-    });
+  //     console.log('res2', response);
+  //   };
+  //   getdata();
+  // }, []);
 
   const handleDateClick = (selectInfo) => {
     let title = prompt('일정 :');
     // title 값이 있을때, 화면에 calendar.addEvent() json형식으로 일정을 추가
     let calendarApi = selectInfo.view.calendar;
     calendarApi.unselect(); // clear date selection
-    // alert('Date: ' + selectInfo.dateStr); // 선택날짜
+    alert('Date: ' + selectInfo.dateStr); // 선택날짜
     if (title) {
       calendarApi.addEvent(
         axios
           .post('http://localhost:8081/api/planSave', {
             id: createEventId(),
             title: title,
-            start_time: selectInfo.dateStr,
-            end_time: selectInfo.dateStr,
+            start: selectInfo.dateStr,
+            end: selectInfo.dateStr,
             allDay: selectInfo.allDay,
           })
           .then(function (res) {
@@ -92,6 +83,19 @@ const Calendar = () => {
         // 이벤트명 : function(){} : 각 날짜에 대한 이벤트를 통해 처리할 내용..
         dayMaxEvents={true}
         weekends={false} //주말
+        events={[
+          axios
+            .get('http://localhost:8081/api/planList')
+            .then((Response) => {
+              const rd = Response.data;
+              console.log(rd);
+              console.log(rd[0]);
+              console.log(rd[1]);
+            })
+            .catch((Error) => {
+              console.log('getError', Error);
+            }),
+        ]}
       />
     </div>
   );
